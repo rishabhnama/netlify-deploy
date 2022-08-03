91a8e3165eac68743fba3767b9a532428298931d
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-08-03 10:05:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Name: {process.env.REACT_APP_NAME}</p>
-        <p>Company: {process.env.REACT_APP_COMPANY}</p>
+        <p>First Name: {process.env.REACT_APP_FIRST_NAME}</p>
+        <p>Last Name: {process.env.REACT_APP_LAST_NAME}</p>
+        <p>Age: {process.env.REACT_APP_AGE}</p>
+        <p>State: {process.env.REACT_APP_STATE}</p>
       </header>
     </div>
   );
